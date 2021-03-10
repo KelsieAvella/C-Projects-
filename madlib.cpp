e56028d538d0ebdaf78cc0f3499ecc668ab8b1c0
KelsieAvella <kavella@carthage.edu>
2021-03-10 12:22:56 -0600

    Add files via upload

---
+++ madlib.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+
+{
+    
+    string word1;
+    string word2;
+    string word3;
+    string word4;
+    string word5;
+    string word6;
+    string word7;
+    
+    cout << "Please enter a noun:" << endl;
+    cin >> word1;
+    
+   cout << "Please enter a verb ending in - ing:" << endl;
+   cin >> word2;
+   
+   cout << "Please enter an adjective:" << endl;
+   cin >> word3;
+   
+   cout << "Please enter a noun:" << endl;
+   cin >> word4;
+   
+   cout << "Please enter a room of a house:" << endl;
+   cin >> word5;
+   
+   cout << "Please enter a number:" << endl;
+   cin>> word6;
+   
+   cout << "Please enter an emotion:" << endl;
+   cin>> word7; 
+   
+   cout << endl;
+   cout << "There was once an old lady who lived in a " << word1 << " by herself. " << "One day she went "<< word2 << " and came across a " << word3 <<  " " << word4 << "!"<< " She loved the " << word4 << " so much that she decided to take it home." << " When she arrived with the " << word4 << " she realized that it would look best in the " << word5 << "." << " She put it there and soon after fell into a deep sleep, but when she woke up the " << word4 << " was gone!" << " She checked every inch of her " << word1 << " " << word6 << " times!" << " No matter how much she looked for the " << word4 << " she could not find it. This made her feel very " <<  word7 << " and confused. For the rest of her life she wondered where her " << word4 << " could have gone and never found out the truth." << endl; 
+   
+   
+  
+    
+    return 0;
+    
+}
